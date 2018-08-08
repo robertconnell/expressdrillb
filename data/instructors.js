250e908c1b88add3faa45c3b2cf34041e624423b
Robert Connell <robertconnell@Kristinas-MacBook-Pro.local>
2018-08-08 09:18:13 -0600

    created express drill b

---
+++ data/instructors.js
@@ -0,0 +1,26 @@
+module.exports = [{
+    id: 1,
+    fullName: "Kyle Coberly",
+    title: "Faculty Director",
+    numberOfDogs: 0
+},{
+    id: 2,
+    fullName: "Danny Fritz",
+    title: "Lead Instructor",
+    numberOfDogs: 0
+},{
+    id: 7,
+    fullName: "Kim Schlesinger",
+    title: "Instructor",
+    numberOfDogs: 0
+},{
+    id: 8,
+    fullName: "Peter Ostiguy",
+    title: "Associate Instructor",
+    numberOfDogs: 1
+},{
+    id: 10,
+    fullName: "Matt Winzer",
+    title: "Resident",
+    numberOfDogs: 2
+}]
